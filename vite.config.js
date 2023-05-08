1d0414d9121df8228fb4947ec7e9b401918afb2c
jacklandwer <land7738@stthomas.edu>
2023-05-07 22:10:26 -0500

    potential link fix 3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cg-illumination/',
+  base: 'jacklandwer/cg-illumination/',
   plugins: [vue()],
   resolve: {
     alias: {
